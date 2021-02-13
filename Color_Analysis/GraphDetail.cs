48d10502b3f2bfcfc27d73034860bfafbe0ef6e6
dys9 <dys9@naver.com>
2021-02-13 13:15:02 +0900

    2021.02.13 (1), Devided to Another Class

---
+++ Color_Analysis/GraphDetail.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Color_Analysis
+{
+    public class GraphDetail
+    {
+        private Chart _chtResult;
+        public Chart chtResult
+        {
+            get { return _chtResult; }
+            set { _chtResult = value;  }
+        }
+
+        private List<double> _LawX = new List<double>();
+        public List<double> LawX
+        {
+            get { return _LawX; }
+            set { _LawX = value; }
+        }
+
+        private List<double> _LawY = new List<double>();
+        public List<double> LawY
+        {
+            get { return _LawY; }
+            set { _LawY = value; }
+        }
+
+        private PointF _PointResult = new PointF(0.57f, 0.42f);// 0.57f, 0.42f // 0.64f, 0.55f
+        public PointF PointResult
+        {
+            get { return _PointResult; }
+            set { _PointResult = value; }
+        }
+
+        public GraphDetail(Chart Cht)
+        {
+            this.chtResult = Cht;
+        }
+
+
+
+        private double CustomCeil(double input)
+        {
+            if (input > 1.0)
+            {
+                return (double)((int)input);
+            }
+
+            double d = input;
+            int cnt = 0;
+            while (d < 1.0)
+            {
+                cnt++;
+                d *= 10;
+            }
+
+            double result = Math.Ceiling(input * Math.Pow(10, cnt)) / Math.Pow(10, cnt);
+            return result;
+        }
+
+        public List<PointF> SortPointPoly(List<double> px, List<double> py)
+        {
+            int size = px.Count;
+            double averageX = 0;
+            double averageY = 0;
+            List<PointF> point = new List<PointF>();
+
+            for (int i = 0; i < size; i++)
+            {
+                averageX += px[i];
+                averageY += py[i];
+                point.Add(new PointF((float)px[i], (float)py[i]));
+            }
+
+            double finalAverageX = averageX / Convert.ToDouble(size);
+            double finalAverageY = averageY / Convert.ToDouble(size);
+
+            point.Sort(delegate (PointF lhs, PointF rhs)
+            {
+                double lhsAngle = Math.Atan2(lhs.Y - finalAverageY, lhs.X - finalAverageX);
+                double rhsAngle = Math.Atan2(rhs.Y - finalAverageY, rhs.X - finalAverageX);
+
+                // Depending on the coordinate system, you might need to reverse these two conditions 
+                if (lhsAngle < rhsAngle)
+                    return -1;
+                if (lhsAngle > rhsAngle)
+                    return 1;
+                return 0;
+            });
+            return point;
+        }
+
+        public void DrawGraph()
+        {
+            if (LawX.Count + LawY.Count > 0)
+            {
+                chtResult.Series.Clear();
+                chtResult.ChartAreas.Clear();
+
+                double Xinterval = CustomCeil(LawX.Max() - LawX.Min());
+                double Yinterval = CustomCeil(LawY.Max() - LawY.Min());
+
+                ChartArea GraphArea = chtResult.ChartAreas.Add("Result");
+                GraphArea.AxisX.Interval = Xinterval;
+                GraphArea.AxisX.Minimum = (LawX.Min() - Xinterval);
+                GraphArea.AxisX.Maximum = (LawX.Max() + Xinterval);
+
+                GraphArea.AxisY.Interval = Yinterval;
+                GraphArea.AxisY.Minimum = (LawY.Min() - Yinterval);
+                GraphArea.AxisY.Maximum = (LawY.Max() + Yinterval);
+
+                if (PointResult.X < GraphArea.AxisX.Minimum)
+                    GraphArea.AxisX.Minimum = ((double)(new decimal(PointResult.X))) - Xinterval;
+                if (PointResult.X > GraphArea.AxisX.Maximum)
+                    GraphArea.AxisX.Maximum = ((double)(new decimal(PointResult.X))) + Xinterval;
+                if (PointResult.Y < GraphArea.AxisY.Minimum)
+                    GraphArea.AxisY.Minimum = ((double)(new decimal(PointResult.Y))) - Yinterval;
+                if (PointResult.Y > GraphArea.AxisY.Maximum)
+                    GraphArea.AxisY.Maximum = ((double)(new decimal(PointResult.Y))) + Yinterval;
+
+                Series Lines = chtResult.Series.Add("Lines"); //새로운 series 생성
+                Lines.IsVisibleInLegend = false;
+                Lines.ChartType = SeriesChartType.Line;
+                Lines.BorderWidth = 2;
+
+                for (int i = 0; i < LawX.Count; i++)
+                {
+                    Lines.Points.AddXY(LawX[i], LawY[i]);
+                    if (i == LawX.Count - 1) Lines.Points.AddXY(LawX[0], LawY[0]);
+                }
+
+                Series pPoint = chtResult.Series.Add("Point");
+                pPoint.ChartType = SeriesChartType.Point;
+                pPoint.IsVisibleInLegend = false;
+                pPoint.Color = Color.Red;
+                pPoint.Points.AddXY((double)PointResult.X, (double)PointResult.Y);
+            }
+        }
+    }
+}
--- Color_Analysis/mainForm.cs
+++ Color_Analysis/mainForm.cs
@@ -9,9 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using static System.Windows.Forms.ComboBox;
+
 // https://stackoverflow.com/questions/42107207/draw-cie-color-space-in-mschart // Draw Graph
 // https://link2me.tistory.com/857 // Communication for Forms
 // https://docs.microsoft.com/ko-kr/dotnet/api/system.drawing.rectangle.contains?view=net-5.0 // isinRect
+// https://blog.hexabrain.net/151 // delegate & event
+
 namespace Color_Analysis
 {
     public partial class mainForm : Form
@@ -35,6 +38,13 @@ namespace Color_Analysis
         {
             get { return _PointResult; }
             set { _PointResult = value; }
+        }
+
+        private GraphDetail _graphDetail;
+        public GraphDetail graphDetail
+        {
+            get { return _graphDetail; }
+            set { _graphDetail = value; }
         }
 
         public const string colorPath = @"D:\이대현\WORK_SPACE\Project_C#\Color_Analysis\Color_Analysis\Color";
@@ -63,6 +73,9 @@ namespace Color_Analysis
             cbLens1T.Items.AddRange(new string[] {"1T", "2T", "3T", "4T", "Input"});
             cbLens2T.Items.AddRange(new string[] {"1T", "2T", "3T", "4T", "Input"});
             cbLens3T.Items.AddRange(new string[] {"1T", "2T", "3T", "4T", "Input"});
+
+            graphDetail = new GraphDetail(chtResult);
+            graphDetail.PointResult = PointResult;
         }
 
         private void btnSet_Click(object sender, EventArgs e)
@@ -123,7 +136,7 @@ namespace Color_Analysis
             LawY.Clear();
             if (cbLaw.SelectedItem.ToString() != null)
             {
-                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(lawPath); 
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(lawPath);
                 foreach (System.IO.FileInfo file in di.GetFiles())
                 {
                     if (cbLaw.SelectedItem.ToString().Equals(file.Name.Replace(".txt", "")))
@@ -132,126 +145,33 @@ namespace Color_Analysis
                         for (var i = 0; i < textValue.Length; i++)
                         {
                             string[] vs = textValue[i].Split(' ');
-                            LawX.Add(Convert.ToDouble(vs[0]));
-                            LawY.Add(Convert.ToDouble(vs[1]));
+                            LawX.Add((double)(new decimal(Convert.ToDouble(vs[0]))));
+                            LawY.Add((double)(new decimal(Convert.ToDouble(vs[1]))));
+
+                            graphDetail.LawX = LawX.ToList();
+                            graphDetail.LawY = LawY.ToList();
                         }
                     }
                 }
             }
-
-            List<PointF> Point_temp = SortPointPoly(LawX, LawY);
-            LawX.Clear();
-            LawY.Clear();
-
-            for (int i = 0; i < Point_temp.Count; i++)
-            {
-                LawX.Add(Convert.ToDouble((double)(new decimal(Point_temp[i].X))));
-                LawY.Add(Convert.ToDouble((double)(new decimal(Point_temp[i].Y))));
-            }
-        }
-
-        private List<PointF> SortPointPoly(List<double> px, List<double> py)
-        {// https://www.crocus.co.kr/1634?category=209527
-            int size = px.Count;
-            double averageX = 0;
-            double averageY = 0;
-            List<PointF> point = new List<PointF>();
-
-            for (int i = 0; i < size; i++)
-            {
-                averageX += px[i];
-                averageY += py[i];
-                point.Add(new PointF((float)px[i], (float)py[i]));
-            }
-
-            double finalAverageX = averageX / Convert.ToDouble(size);
-            double finalAverageY = averageY / Convert.ToDouble(size);
-
-            point.Sort(delegate (PointF lhs, PointF rhs)
-            {
-                double lhsAngle = Math.Atan2(lhs.Y - finalAverageY, lhs.X - finalAverageX);
-                double rhsAngle = Math.Atan2(rhs.Y - finalAverageY, rhs.X - finalAverageX);
-
-                // Depending on the coordinate system, you might need to reverse these two conditions 
-                if (lhsAngle < rhsAngle)
-                    return -1;
-                if (lhsAngle > rhsAngle)
-                    return 1;
-                return 0;
-            });
-            return point;
-        }
-
-        private void DrawGraph()
-        {
-            if (LawX.Count + LawY.Count > 0)
-            {
-                chtResult.Series.Clear();
-                chtResult.ChartAreas.Clear();
-
-                double Xinterval = CustomCeil(LawX.Max() - LawX.Min());
-                double Yinterval = CustomCeil(LawY.Max() - LawY.Min());
-
-                ChartArea GraphArea = chtResult.ChartAreas.Add("Result");
-                GraphArea.AxisX.Interval = Xinterval;
-                GraphArea.AxisX.Minimum = (LawX.Min() - Xinterval);
-                GraphArea.AxisX.Maximum = (LawX.Max() + Xinterval);
-
-                GraphArea.AxisY.Interval = Yinterval;
-                GraphArea.AxisY.Minimum = (LawY.Min() - Yinterval);
-                GraphArea.AxisY.Maximum = (LawY.Max() + Yinterval);
-
-
-                if (PointResult.X < GraphArea.AxisX.Minimum)
-                    GraphArea.AxisX.Minimum = ((double)(new decimal(PointResult.X))) - Xinterval;
-                if (PointResult.X > GraphArea.AxisX.Maximum)
-                    GraphArea.AxisX.Maximum = ((double)(new decimal(PointResult.X))) + Xinterval;
-                if (PointResult.Y < GraphArea.AxisY.Minimum)
-                    GraphArea.AxisY.Minimum = ((double)(new decimal(PointResult.Y))) - Yinterval;
-                if (PointResult.Y > GraphArea.AxisY.Maximum)
-                    GraphArea.AxisY.Maximum = ((double)(new decimal(PointResult.Y))) + Yinterval;
-
-                Series Lines = chtResult.Series.Add("Lines"); //새로운 series 생성
-                Lines.IsVisibleInLegend = false;
-                Lines.ChartType = SeriesChartType.Line;
-                Lines.BorderWidth = 2;
-
-                for (int i = 0; i < LawX.Count; i++)
-                {
-                    Lines.Points.AddXY(LawX[i], LawY[i]);
-                    if (i == LawX.Count - 1) Lines.Points.AddXY(LawX[0], LawY[0]);
-                }
-
-                Series pPoint = chtResult.Series.Add("Point");
-                pPoint.ChartType = SeriesChartType.Point;
-                pPoint.IsVisibleInLegend = false;
-                pPoint.Color = Color.Red;
-                pPoint.Points.AddXY((double)PointResult.X, (double)PointResult.Y);
-            }
-        }
-
-        private double CustomCeil(double input)
-        {
-            if ( input > 1.0)
-            {
-                return (double)((int)input);
-            }
-
-            double d = input;
-            int cnt = 0;
-            while (d < 1.0)
-            {
-                cnt++;
-                d *= 10;
-            }
-
-            double result = Math.Ceiling(input * Math.Pow(10, cnt)) / Math.Pow(10, cnt);
-            return result;
         }
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
-            DrawGraph();
+            List<PointF> Point_temp = graphDetail.SortPointPoly(graphDetail.LawX, graphDetail.LawY);
+            graphDetail.LawX.Clear();
+            graphDetail.LawY.Clear();
+
+            string temp = "";
+            for (int i = 0; i < Point_temp.Count; i++)
+            {
+                graphDetail.LawX.Add(Convert.ToDouble((double)(new decimal(Point_temp[i].X))));
+                graphDetail.LawY.Add(Convert.ToDouble((double)(new decimal(Point_temp[i].Y))));
+                temp += $"({graphDetail.LawX[i]}, {graphDetail.LawY[i]})\r\n";
+            }
+
+            graphDetail.DrawGraph();
+            graphDetail.chtResult.Show();
         }
     }
 }
